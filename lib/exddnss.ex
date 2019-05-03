cbbd0bd05322d047c1e9b8c857e7f4f7751dd874
Christian Taedcke <hacking@taedcke.com>
2019-05-03 18:25:22 +0200

    Initial commit of files from "mix new --sup"

---
+++ lib/exddnss.ex
@@ -0,0 +1,18 @@
+defmodule Exddnss do
+  @moduledoc """
+  Documentation for Exddnss.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exddnss.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exddnss_test.exs
@@ -0,0 +1,8 @@
+defmodule ExddnssTest do
+  use ExUnit.Case
+  doctest Exddnss
+
+  test "greets the world" do
+    assert Exddnss.hello() == :world
+  end
+end
